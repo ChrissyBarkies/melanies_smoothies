ba5f51a993622657a849819bbd1a637ce4d3cb21
ChrissyBarkies <90527482+ChrissyBarkies@users.noreply.github.com>
2026-03-20 17:58:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,15 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
+200
+r.headers['content-type']
+'application/json; charset=utf8'
+r.encoding
+'utf-8'
+r.text
+'{"type":"User"...'
+r.json()
+{'private_gists': 419, 'total_private_repos': 77, ...}
